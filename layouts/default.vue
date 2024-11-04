7980cfa46f7a6d71bb56eb4772861b58bca51b13
strukovd <dmitryi97@yandex.ru>
2024-11-04 15:42:02 +0600

    added simple page and laouyts

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <VApp>
+	<header>
+		Default layout header
+	</header>
+	<main>
+		<slot/>
+	</main>
+	<footer>
+		Default layout footer
+	</footer>
+  </VApp>
+</template>
+<script lang="ts">
+
+</script>
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    main page
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
