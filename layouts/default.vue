54f756f5321140cdb573575a5e39721c886c9eae
strukovd <dmitryi97@yandex.ru>
2024-11-05 16:09:43 +0600

    added editorconfig

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <section id="default-layout">
-	<header>
-		[Default layout header]
-	</header>
-	<main>
-		<slot/>
-	</main>
-	<footer>
-		[Default layout footer]
-	</footer>
+    <header>
+      [Default layout header]
+    </header>
+    <main>
+      <slot/>
+    </main>
+    <footer>
+      [Default layout footer]
+    </footer>
   </section>
 </template>
 <script lang="ts">
