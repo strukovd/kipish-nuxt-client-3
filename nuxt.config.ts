30827c7959d97fab81a51b53133958cdb6f3a4a6
strukovd <dmitryi97@yandex.ru>
2024-12-06 02:03:07 +0600

    fix aos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  modules: [
-    '@pinia/nuxt'
-  ],
+  modules: ['@pinia/nuxt', 'nuxt-aos'],
 
   css: [ "vuetify/styles/main.sass" ],
   build: {
