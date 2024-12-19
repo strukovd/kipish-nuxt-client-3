c6b04fe7b01a7046577ea55037cc2a77003018a3
strukovd <dmitryi97@yandex.ru>
2024-12-19 21:29:12 +0600

    fix layouts

---
+++ layouts/default-mobile.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="default-layout">
+    <ClientOnly>
+      <AppMobileHeader/>
+    </ClientOnly>
+    <main style="padding-bottom:6em;">
+      <slot/>
+    </main>
+    <ClientOnly>
+      <AppFooterMobile/>
+    </ClientOnly>
+  </section>
+</template>
+
+<script lang="ts">
+import AppMobileHeader from '~/components/common/AppMobileHeader.vue';
+import AppFooterMobile from '~/components/common/AppFooterMobile.vue';
+
+export default {
+  components: { AppMobileHeader, AppFooterMobile },
+};
+</script>
+
+<style>
+.default-layout {
+  position:relative;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
   <section id="default-layout">
-    <AppHeader/>
+    <ClientOnly>
+      <AppHeader/>
+    </ClientOnly>
     <main style="min-height:600px; padding-bottom:6em;">
       <slot/>
     </main>
-    <AppFooter/>
+    <ClientOnly>
+      <AppFooter/>
+    </ClientOnly>
   </section>
 </template>
+
 <script lang="ts">
 import AppHeader from '~/components/common/AppHeader.vue';
 import AppFooter from '~/components/common/AppFooter.vue';
@@ -14,8 +19,8 @@ import AppFooter from '~/components/common/AppFooter.vue';
 export default {
   components: { AppHeader, AppFooter },
 };
-
 </script>
+
 <style>
 .default-layout {
   position:relative;
