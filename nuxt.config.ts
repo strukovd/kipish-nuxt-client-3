62eb1e17b5bf5635c14cfdd6e8a8e9adb359b3ce
strukovd <dmitryi97@yandex.ru>
2024-12-26 01:57:35 +0600

    fix og:image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,29 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', },
+      ],
+      meta: [
+        { name: 'og:image', content: 'https://kipish.kg/favicon.svg' },
+        { name: 'og:image:alt', content: 'Kipish.kg - Медиа ресурс о светской и ночной жизни в Бишкеке' },
+        { name: 'og:image:width', content: '60' },
+        { name: 'og:image:height', content: '56' },
+      ]
+    },
+  },
+
+  nitro: {
+    routeRules: {
+      '/**': { prerender: false },
+    },
+  },
+
+
   modules: ['@pinia/nuxt', 'nuxt-aos'],
   plugins: [
     '~/plugins/global-components',
