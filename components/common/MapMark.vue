c7881b889a8382e051477f3984fd34c7e73f072d
strukovd <dmitryi97@yandex.ru>
2024-12-29 04:07:10 +0600

    added map

---
+++ components/common/MapMark.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div :class="dark ? 'pin-dark' : 'pin-light'">
+      <img :src="dark ? '/static/images/logo.svg' : '/static/images/logo2.svg'" width="26px" height="26px"/>
+    </div>
+    <div :class="dark ? 'pulse-dark' : 'pulse-light'"></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    dark: { type: Boolean, default: false }
+  }
+});
+</script>
+
+<style lang="scss">
+.pin-dark, .pin-light {
+  width: 30px;
+  height: 30px;
+  border-radius: 50% 50% 50% 0;
+  position: absolute;
+  // transform: rotate(-45deg);
+  left: 50%;
+  top: 50%;
+  margin: -20px 0 0 -20px;
+  animation-name: bounce;
+  animation-fill-mode: both;
+  animation-duration: 1s;
+  padding: 2px;
+}
+
+.pin-dark {
+  background: white;
+}
+
+.pin-light {
+  background: black;
+}
+
+.pulse-dark, .pulse-light {
+  background: rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  height: 14px;
+  width: 14px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin: 11px 0 0 -12px;
+  transform: rotateX(55deg);
+  z-index: -2;
+
+  &:after {
+    content: "";
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    position: absolute;
+    margin: -13px 0 0 -13px;
+    animation: pulsate 1s ease-out infinite;
+    opacity: 0.0;
+    box-shadow: 0 0 1px 2px #89849b;
+    animation-delay: 1.1s;
+  }
+}
+
+@keyframes pulsate {
+  0% {
+    transform: scale(0.1, 0.1);
+    opacity: 0.0;
+  }
+  50% {
+    opacity: 1.0;
+  }
+  100% {
+    transform: scale(1.2, 1.2);
+    opacity: 0;
+  }
+}
+
+@keyframes bounce {
+  0% {
+    opacity: 0;
+    transform: translateY(-2000px) rotate(-45deg);
+  }
+  60% {
+    opacity: 1;
+    transform: translateY(30px) rotate(-45deg);
+  }
+  80% {
+    transform: translateY(-10px) rotate(-45deg);
+  }
+  100% {
+    transform: translateY(0) rotate(-45deg);
+  }
+}
+</style>
