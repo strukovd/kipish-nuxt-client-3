95a932e09f12aa4d326fa4f167432a6ba5066a01
strukovd <dmitryi97@yandex.ru>
2025-01-14 01:37:52 +0600

    fixed arrow

--- stores/AppStore.ts
+++ stores/AppStore.ts
@@ -11,6 +11,7 @@ export const useAppStore = defineStore('app', {
 
     videoDomain: process.env.NODE_ENV === 'production' ? '/api/v1/reports/video/stream/' : 'http://localhost:8083/api/v1/reports/video/stream/',
     windowWidth: 0,
+    shouldShowScrollArrow: undefined as any,
     isMobile: false,
     currentCity: import.meta.browser ? JSON.parse(localStorage.getItem('city') ?? 'null') : {"id":1,"nameRu":"Бишкек","nameKg":"Бишкек","nameEn":"Bishkek","visible":true},
     sourceId: import.meta.browser ? JSON.parse(localStorage.getItem('sourceId') ?? 'null') : null,
